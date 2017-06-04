81715a3c2c39ea5a603d988ede20c33f6e1c941a
Anton Bozhiy <abozhiy@gmail.com>
2017-06-04 12:58:54 +0300

    add migrations for users, teams and users_team tables

---
+++ db/migrate/20170530202604_create_users_teams.rb
@@ -0,0 +1,9 @@
+class CreateUsersTeams < ActiveRecord::Migration
+  def change
+    create_table :users_teams do |t|
+      t.integer :user_id
+      t.integer :team_id
+      t.timestamps null: false
+    end
+  end
+end
