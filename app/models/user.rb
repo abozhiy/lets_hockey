a0ed2353d805a2746aa60b60a4084a5ba4bf72d6
Anton Bozhiy <abozhiy@gmail.com>
2017-06-18 17:13:37 +0300

    fix User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,22 @@ class User < ActiveRecord::Base
   has_many :users_teams
   has_many :teams, through: :users_teams
 
-  acts_as_authentic do |c|
-    c.login_field = :login
-    c.require_password_confirmation = false
-    c.crypto_provider = Authlogic::CryptoProviders::Sha512
-    c.perishable_token_valid_for = 1.month
-    c.validate_email_field = true
+  before_validation :set_login
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  validates :password, length: { minimum: 6 }
+
+  acts_as_authentic do |act|
+    act.require_password_confirmation = false
+    act.validate_email_field = true
+    # act.crypto_provider = Authlogic::CryptoProviders::Sha512
+    # act.perishable_token_valid_for = 3.month
+  end
+
+  private
+
+  def set_login
+    self.login = self.email
   end
 end
