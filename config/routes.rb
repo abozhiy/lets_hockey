ed468a3170297520a851f3680f937c0c68f36c1a
Anton Bozhiy <abozhiy@gmail.com>
2017-06-25 14:45:18 +0300

    root_path is start#redirect; add teams to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
 
-  root 'users#index'
+  root 'start#redirect'
 
   resource :user_sessions, path: 'sessions', only: [:new, :create, :destroy]
 
   resources :users
+
+  resources :teams do
+    member do
+      put :add_user
+    end
+  end
+
 end
